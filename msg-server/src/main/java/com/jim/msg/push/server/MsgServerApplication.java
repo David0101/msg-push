9e35b3db45db300c38d67be7518f6f21e0776e7c
jim <ljw5332545>
2020-07-31 18:51:39 +0800

    MsgServerApplication

--- msg-server/src/main/java/com/jim/msg/push/server/MsgServerApplication.java
+++ msg-server/src/main/java/com/jim/msg/push/server/MsgServerApplication.java
@@ -2,7 +2,10 @@ package com.jim.msg.push.server;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -25,4 +28,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableAsync
 public class MsgServerApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MsgServerApplication.class);
+
+//        app.addListeners(new ApplicationPidFileWriter("./msg-server.pid"),new ApplicationPortFileWriter("./msg-server.port"));
+//        app.setWebApplicationType(WebApplicationType.NONE);
+//        app.run(args);
+//        log.info("MsgServerApplication 启动");
+    }
 }
