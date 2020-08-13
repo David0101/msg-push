5d332eac9d7857c47a5cb9c08fc52544b60a4c75
jim <ljw5332545>
2020-08-13 18:07:35 +0800

    redis 版本兼容问题

--- msg-rounter/src/main/java/com/jim/msg/push/rounter/config/RedisConfig.java
+++ msg-rounter/src/main/java/com/jim/msg/push/rounter/config/RedisConfig.java
@@ -1,22 +1,36 @@
 package com.jim.msg.push.rounter.config;
 
-import com.jim.msg.push.rounter.cache.RedisClient;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.jim.msg.push.rounter.cache.RedisClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPoolConfig;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 
 /**
  * @program: msg-push
  * @author: jim
  * @create: 2020-08-05 14:48
  */
-@Component
-@Lazy(value = false)
+//@Component
+//@Lazy(value = false)
 @Configuration
 public class RedisConfig {
     private static  final Logger LOG = LoggerFactory.getLogger(RedisConfig.class);
@@ -29,15 +43,80 @@ public class RedisConfig {
     @Value("${redis.database}")
     private int database;
 
-    @PostConstruct
-    public void init() {
-        initRedis();
+    @Value("${redis.pool.maxTotal}")
+    private int maxTotal;
+    @Value("${redis.pool.minIdle}")
+    private int minIdle;
+    @Value("${redis.pool.maxWaitMillis}")
+    private int maxWaitMillis;
+
+
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("r-wz9l1uaq7hbkntiin4pd.redis.rds.aliyuncs.com", 6379);
+//        config.setPassword("RfyS8wJZCrM3");
+//        config.setDatabase(2);
+//        return new JedisConnectionFactory(config);
+//    }
+
+    @Bean
+    @ConfigurationProperties()
+    public JedisPoolConfig jedisPoolConfig(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(maxTotal);
+        jedisPoolConfig.setMinIdle(minIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        return new JedisPoolConfig();
+
     }
 
-    private void initRedis() {
-        LOG.info("----------init redis start-----");
-        RedisClient.init(server, port, password,database);
-        LOG.info("----------init redis end-----");
+    @Bean
+    public JedisClientConfiguration jedisClientConfiguration(){
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .readTimeout(Duration.ofSeconds(10))
+                .usePooling().poolConfig(jedisPoolConfig()).build();
+        return jedisClientConfiguration;
     }
 
+
+    @Bean
+    @ConfigurationProperties(prefix="redis")
+    public RedisStandaloneConfiguration redisStandaloneConfiguration(){
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setPassword(password);
+        redisConfiguration.setHostName(server);
+        redisConfiguration.setDatabase(database);
+        redisConfiguration.setPort(port);
+        return redisConfiguration;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedisStandaloneConfiguration RedisStandaloneConfiguration){
+        RedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory(RedisStandaloneConfiguration);
+        return redisConnectionFactory;
+    }
+
+    @Bean("redisTemplate")
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+        template.setValueSerializer(serializer);
+
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+
+    }
 }
--- msg-server/src/main/java/com/jim/msg/push/server/handle/SocketHandler.java
+++ msg-server/src/main/java/com/jim/msg/push/server/handle/SocketHandler.java
@@ -1,21 +1,25 @@
 package com.jim.msg.push.server.handle;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.annotation.OnConnect;
 import com.corundumstudio.socketio.annotation.OnDisconnect;
 
 
 import com.jim.msg.push.commons.util.JacksonUtil;
-import com.jim.msg.push.rounter.cache.RedisClient;
+//import com.jim.msg.push.rounter.cache.RedisClient;
 import com.jim.msg.push.rounter.commons.SocketIOHelper;
 import com.jim.msg.push.server.common.SocketIOConst;
 import com.jim.msg.push.server.dto.SocketIOSessionDto;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -29,6 +33,9 @@ public class SocketHandler {
 
     @Autowired
     private SocketIOHelper helper;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    public static String BASE_TOP_COURSE_KEY = "base:topcourse:*";
     @OnConnect
     public void onConnect(SocketIOClient socketIOClient){
         String sessionId = socketIOClient.getSessionId().toString();
@@ -38,8 +45,11 @@ public class SocketHandler {
             SocketIOSessionDto socketIOSessionDto = new SocketIOSessionDto(sessionId,userName,helper.getLocalConsumerQueue());
             String sessionJson = JacksonUtil.obj2String(socketIOSessionDto);
             String sessionIdKey = helper.getCachSessionKey(sessionId);
-            String jsonStr = RedisClient.getValue( "yunzhangxiao:department:userId:444");
-            RedisClient.setValue(sessionIdKey,sessionJson, SocketIOConst.SESSION_TIME_OUT_SECOND);
+            Set<String> keys = redisTemplate.keys(BASE_TOP_COURSE_KEY);
+            redisTemplate.opsForValue().set("111","222");
+            Object test = redisTemplate.opsForValue().get("111");
+//            Object jsonStr = RedisClient.getValue( "yunzhangxiao:department:userId:000502b4-34a3-4245-9239-2d0f2e37d1f2");
+//            RedisClient.setValue(sessionIdKey,sessionJson, SocketIOConst.SESSION_TIME_OUT_SECOND);
             int a = 0;
         }
         int a;
